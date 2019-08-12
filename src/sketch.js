9ef51b76e0e0c2bcc25ee2dc1f499c9e0d56b0a2
YunchengHua <hua_yuncheng@hotmail.com>
2019-08-12 17:39:15 +0800

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -3,6 +3,7 @@ let points = [];
 let font;
 let words = "ASMR"
 let pos;
+let clevel = 0;
 
 function preload() {
   font = loadFont("tt.ttf");
@@ -10,7 +11,10 @@ function preload() {
 
 
 function setup() {
-  createCanvas(1024, 720);
+  //createCanvas(windowWidth, windowHeight);
+  let c = createCanvas(1920,1080);
+  c.position(windowWidth/2-width/2,windowHeight/2-height/2);
+
   noFill();
 
   mic = new p5.AudioIn();
@@ -18,42 +22,48 @@ function setup() {
   fft = new p5.FFT();
   fft.setInput(mic);
 
-  let font_size = height / 2
+  let font_size = 400
   let bbox = font.textBounds(words, 10, 30, font_size);
   textSize(font_size)
   textFont(font)
   pos = createVector();
-  pos.x = width / 2 - bbox.w / 2
-  pos.y = height / 2 + bbox.h / 3
+  pos.x = - bbox.w / 2
+  pos.y = bbox.h / 3
 
-  points = font.textToPoints(words, pos.x,pos.y, font_size, {
-    sampleFactor: 0.1
+  points = font.textToPoints(words, pos.x, pos.y, font_size, {
+    sampleFactor: 0.2
   });
   console.log(points.length)
 }
 
 function draw() {
   background(100);
-
   let spectrum = fft.analyze();
   let level = mic.getLevel();
-  level = map(level, 0, 1, 0, 255);
+  level = map(level, 0, 1, 0.6, 3);
+  clevel += (level-clevel)/20;
+  translate(width/2,height/2)
+  scale(clevel);
 
-  for (i = 0; i < spectrum.length; i++) {
-    let h = map(spectrum[i], 0, 255, 0, height / 4);
-    stroke(0, level);
-    line(i, height / 2 + h, i, height / 2 - h);
-  }
 
+  // for (i = 0; i < spectrum.length; i++) {
+  //   let h = map(spectrum[i], 0, 255, 0, height / 4);
+  //   stroke(0, level);
+  //   line(i, height / 2 + h, i, height / 2 - h);
+  // }
+
+  strokeWeight(8);
   for (let i = 0; i < points.length; i++) {
     stroke(255)
-    let l = 10;
+
+    let l = spectrum[i]/15.0;
     let x = points[i].x;
     let y = points[i].y;
-    let a = PI*(points[i].alpha+90)/180;
+    let a = PI * (points[i].alpha + 90) / 180;
     line(x-l*cos(a), y-l*sin(a), x+l*cos(a), y+l*sin(a));
+    //line(x, y, x + l * cos(a), y + l * sin(a));
   }
 
   fill(255)
-  text(words,pos.x,pos.y)
+  text(words, pos.x, pos.y)
 }
